939891c4ce5a2d5ab43a055ea513b4d1846f9c9f
leonardo mori <leonardomfl96@gmail.com>
2017-10-10 02:26:34 -0300

    concluído

--- src/main.c
+++ src/main.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
+#include<string.h>
 
 int main() {
-  int a[5];
-  int n;
+  int a[5]; 
+  int n, i;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+  for (int i=1; i<=n; i++) {  /* Imprime resultado da n-esima iteracao do scheduler */
+  
+  	if(i%a[0]==0){
+  		printf("0");
+	  }
+  	if(i%a[1]==0){
+  		printf("1");
+	  }	  
+  	if(i%a[2]==0){
+  		printf("2");
+	  }
+  	if(i%a[3]==0){
+  		printf("3");
+	  }
+  	if(i%a[4]==0){
+  		printf("4");
+	  }	
+	else if (i%a[0]!=0 && i%a[1]!=0 && i%a[2]!=0 && i%a[3]!=0 && i%a[4]!=0){
+		printf("-");
+	}
+	printf("\n");    	    
   }
 
   return 0;
